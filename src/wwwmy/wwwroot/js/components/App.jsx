f764da0e40f72cbcd93c0b45b36126426d0bdc0b
Paul Byron <syntheticforms@gmail.com>
2016-11-04 21:24:06 +0300

    Структура для react

---
+++ src/wwwmy/wwwroot/js/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default App = () => {
+    return (
+        <div id="wrapper">
+            <div id="header">
+                HEADER
+            </div>
+            <div id="body">
+                BODY
+            </div>
+            <div id="footer">
+                FOOTER
+            </div>
+        </div>
+    )
+}
